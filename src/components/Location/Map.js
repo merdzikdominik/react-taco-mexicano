ba5e23d8c609b834c19bc02b7da1d7ecd897ade3
Dominik <dominik.merdzik@onet.pl>
2022-10-03 23:48:33 +0200

    fixed loading screen in location section

--- src/components/Location/Map.js
+++ src/components/Location/Map.js
@@ -6,8 +6,8 @@ const Iframe = styled.iframe`
     width: 100%;
     height: 60%;
     position: absolute;
-    -webkit-box-shadow: ${props => props.loading ? 'none' : '8px 8px 24px 0px rgba(66, 68, 90, 1)' };
-    -moz-box-shadow: ${props => props.loading ? 'none' : '8px 8px 24px 0px rgba(66, 68, 90, 1)' };
+    -webkit-box-shadow: ${props => props.$loading ? 'none' : '8px 8px 24px 0px rgba(66, 68, 90, 1)' };
+    -moz-box-shadow: ${props => props.$loading ? 'none' : '8px 8px 24px 0px rgba(66, 68, 90, 1)' };
     box-shadow: ${props => props.$loading ? 'none' : '8px 8px 24px 0px rgba(66, 68, 90, 1)' };
 `;
 
